e01426de56ded7475bbb87da235e85b08b54fbc9
Rizqiev Prakozov <bryarizqiev@gmail.com>
2022-12-25 13:16:37 +0700

    Second

--- src/main/java/main/utility/JDBCUtil.java
+++ src/main/java/main/utility/JDBCUtil.java
@@ -9,8 +9,8 @@ public class JDBCUtil {
     public static Connection getConnection() throws SQLException {
         String hostNPort = "103.171.85.233:3306";
         String database = "ParkirDB";
-        String user = "bryan1";
-        String password = "${Password12345678}";
+        String user = "usermu";
+        String password = "passwordmu";
 
         return DriverManager.getConnection("jdbc:mysql://" + hostNPort + "/" + database + "?useSSL=false", user, password);
     }
