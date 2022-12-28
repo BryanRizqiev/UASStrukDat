7d200c7ce2755a06c0bad4a5e6a5e980bb7a0142
Ahmad Dzaky <dzakym4343@gmail.com>
2022-12-29 04:54:24 +0700

    ada

--- src/main/java/main/UI_Main.java
+++ src/main/java/main/UI_Main.java
@@ -2,7 +2,6 @@ package main;
 
 import main.view.panelMasuk;
 import main.view.panelKeluar;
-import main.view.panelShow;
 
 public class UI_Main extends javax.swing.JFrame {
 
@@ -65,7 +64,7 @@ public class UI_Main extends javax.swing.JFrame {
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
         src.setViewportView(new panelMasuk());
     }//GEN-LAST:event_jMenuItem1ActionPerformed
-    
+
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
         src.setViewportView(new panelKeluar());
     }//GEN-LAST:event_jMenuItem2ActionPerformed
@@ -84,21 +83,21 @@ public class UI_Main extends javax.swing.JFrame {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
-                    
+
                 }
             }
         } catch (ClassNotFoundException ex) {
             java.util.logging.Logger.getLogger(UI_Main.class
                     .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-            
+
         } catch (InstantiationException ex) {
             java.util.logging.Logger.getLogger(UI_Main.class
                     .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-            
+
         } catch (IllegalAccessException ex) {
             java.util.logging.Logger.getLogger(UI_Main.class
                     .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-            
+
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(UI_Main.class
                     .getName()).log(java.util.logging.Level.SEVERE, null, ex);
--- src/main/java/main/utility/JDBCUtil.java
+++ src/main/java/main/utility/JDBCUtil.java
@@ -9,8 +9,8 @@ public class JDBCUtil {
     public static Connection getConnection() throws SQLException {
         String hostNPort = "103.171.85.233:3306";
         String database = "ParkirDB";
-        String user = "strukdat";
-        String password = "Passwordegan**1234";
+        String user = "user";
+        String password = "pass";
 
         return DriverManager.getConnection("jdbc:mysql://" + hostNPort + "/" + database + "?useSSL=false", user, password);
     }
